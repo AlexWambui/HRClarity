b909141bfdea9158014e94f54ac4ab2773a6e252
Alex <alexwambuik@gmail.com>
2021-04-13 00:04:38 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home | Login</title>
+</head>
+<body>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-sm-5">
+            <div class="card">
+                <h4 class="card-header text-center">Login</h4>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <input type="text" name="username" id="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" id="password" class="form-control">
+                        </div>
+                        <button class="btn btn-success btn-block">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
