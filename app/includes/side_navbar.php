609a232c3429b78fb66d3ff6de97daafe804bd1f
Alex <alexwambuik@gmail.com>
2021-04-13 13:00:39 +0300

    created the side nav bar prototype

---
+++ app/includes/side_navbar.php
@@ -0,0 +1,8 @@
+<section class="side_nav_bar">
+    <ul class="nav_links">
+        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-dashboard"></span> Dashboard</a></li>
+        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-building"></span> Users</a></li>
+        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-users"></span> Departments</a></li>
+        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-attach_money"></span> Occupations</a></li>
+    </ul>
+</section>
