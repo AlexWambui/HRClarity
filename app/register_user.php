4ce60002c26d2b28464bf90a0938d485c3154a92
Alex <alexwambuik@gmail.com>
2021-04-13 23:55:45 +0300

    added the protection for the page so only logged in users can view

--- app/register_user.php
+++ app/register_user.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'includes/functions.php';
 require_once "includes/db_connection.php";
+protect_page();
 if(isset($_REQUEST['first_name'])){
     $first_name = $_REQUEST['first_name'];
     $last_name = $_REQUEST['last_name'];
