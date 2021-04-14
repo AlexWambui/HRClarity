d29c3f33c2b2ac14ba3e9e3dc782d884914a688d
Alex <alexwambuik@gmail.com>
2021-04-14 10:50:19 +0300

    added functionalities to the side navbar like a dropdown

--- app/includes/side_navbar.php
+++ app/includes/side_navbar.php
@@ -1,8 +1,41 @@
 <section class="side_nav_bar">
     <ul class="nav_links">
-        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-dashboard"></span> Dashboard</a></li>
-        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-building"></span> Users</a></li>
-        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-users"></span> Departments</a></li>
-        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-attach_money"></span> Occupations</a></li>
+        <li class="nav_items"><a href="welcome_page.php"><span class="side_nav_icons icon-dashboard"></span> Dashboard</a></li>
+        <?php if($_SESSION['user_level'] == 3 or $_SESSION['user_level'] == 2 ): ?>
+        <li class="nav_items">
+            <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-users"></span> Employees <span class="icon-caret-down"></span></button>
+            <div class="dropdown-container side_nav_dropdown_container">
+                <a href="view_users.php">View employees</a>
+                <a href="register_user.php">Add employee</a>
+            </div>
+        </li>
+            <li class="nav_items">
+                <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-group_add"></span> Employees <span class="icon-caret-down"></span></button>
+                <div class="dropdown-container side_nav_dropdown_container">
+                    <a href="view_users.php">View employees</a>
+                    <a href="register_user.php">Add employee</a>
+                </div>
+            </li>
+        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-group_work"></span> Departments</a></li>
+        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-play_for_work"></span> Occupations</a></li>
+        <?php endif; ?>
     </ul>
 </section>
+
+<script>
+    //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+        dropdown[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } else {
+                dropdownContent.style.display = "block";
+            }
+        });
+    }
+</script>
