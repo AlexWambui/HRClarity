5c6a76cf3e04c17e92e2bc907e9f5dd9484c4a7a
Alex <alexwambuik@gmail.com>
2021-04-14 11:20:38 +0300

    added more features in the side navbar like leaves and recruitments

--- app/includes/side_navbar.php
+++ app/includes/side_navbar.php
@@ -2,25 +2,35 @@
     <ul class="nav_links">
         <li class="nav_items"><a href="welcome_page.php"><span class="side_nav_icons icon-dashboard"></span> Dashboard</a></li>
         <?php if($_SESSION['user_level'] == 3 or $_SESSION['user_level'] == 2 ): ?>
-        <li class="nav_items">
-            <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-users"></span> Employees <span class="icon-caret-down"></span></button>
-            <div class="dropdown-container side_nav_dropdown_container">
-                <a href="view_users.php">View employees</a>
-                <a href="register_user.php">Add employee</a>
-            </div>
-        </li>
             <li class="nav_items">
-                <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-group_add"></span> Employees <span class="icon-caret-down"></span></button>
+                <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-group"></span> Employees <span class="icon-caret-down"></span></button>
                 <div class="dropdown-container side_nav_dropdown_container">
                     <a href="view_users.php">View employees</a>
                     <a href="register_user.php">Add employee</a>
                 </div>
             </li>
-        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-group_work"></span> Departments</a></li>
-        <li class="nav_items"><a href="#"><span class="side_nav_icons icon-play_for_work"></span> Occupations</a></li>
+            <li class="nav_items">
+                <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-group_work"></span> Departments <span class="icon-caret-down"></span></button>
+                <div class="dropdown-container side_nav_dropdown_container">
+                    <a href="view_departments.php">View departments</a>
+                    <a href="add_department.php">Add department</a>
+                </div>
+            </li>
+            <li class="nav_items">
+                <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-work"></span> Occupations <span class="icon-caret-down"></span></button>
+                <div class="dropdown-container side_nav_dropdown_container">
+                    <a href="view_occupations.php">View occupations</a>
+                    <a href="add_occupation.php">Add occupation</a>
+                </div>
+            </li>
+            <li class="nav_items"><a href="leaves.php"><span class="side_nav_icons icon-time_to_leave"></span> Leaves</a></li>
+            <li class="nav_items"><a href="recruitments.php"><span class="side_nav_icons icon-folder-open"></span> Recruitments</a></li>
+            <li class="nav_items"><a href="recruitments.php"><span class="side_nav_icons icon-folder-open"></span> Recruitments</a></li>
         <?php endif; ?>
     </ul>
 </section>
+
+
 
 <script>
     //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
