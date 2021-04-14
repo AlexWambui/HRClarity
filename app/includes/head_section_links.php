021c78b6e0221a20815c29a9f7dc8fc82c0e52c8
Alex <alexwambuik@gmail.com>
2021-04-14 23:21:44 +0300

    added the link to the jqueryTabledata styles

--- app/includes/head_section_links.php
+++ app/includes/head_section_links.php
@@ -3,6 +3,7 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 <link rel="stylesheet" href="../assets/css/icomoon.css">
+<link rel="stylesheet" href="../assets/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="../assets/css/styles.css">
 <script src="../assets/js/jquery.min.js"></script>
 <script src="../assets/js/jquery.dataTables.min.js"></script>
