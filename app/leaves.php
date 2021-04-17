88c8bb0ed6ea7a1c43aeb36bd3a3a9a039aa4b30
Alex <alexwambuik@gmail.com>
2021-04-17 12:28:54 +0300

    Update leaves.php
    used from_date instead of start_date to avoid the problem causing pages not to submit

--- app/leaves.php
+++ app/leaves.php
@@ -3,15 +3,15 @@ include_once 'includes/functions.php';
 include_once 'includes/db_connection.php';
 protect_page();
 
-if(isset($_REQUEST['start_date'])){
+if(isset($_REQUEST['from_date'])){
     $type = $_REQUEST['leave_type'];
     $from = $_REQUEST['from_date'];
     $to = $_REQUEST['to_date'];
     $user_id = $_SESSION['id'];
 
     $sql_add_leave = mysqli_prepare($db_conn, "INSERT INTO leaves (`leave_type`, `from_date`, `to_date`, `user_id`) VALUES (?,?,?,?)");
-    mysqli_stmt_bind_param($sql_add_leave, "sssi", $dpt_name);
-    mysqli_stmt_execute($sql_add_leave);
+    mysqli_stmt_bind_param($sql_add_leave, "sssi", $type, $from, $to, $user_id);
+    mysqli_stmt_execute($sql_add_leave) or die(mysqli_stmt_error($sql_add_leave));
     mysqli_close($db_conn);
     setcookie('success', "leave sent", time()+2);
     header('location: leaves.php');
@@ -52,7 +52,7 @@ include_once 'includes/side_navbar.php';
                             <td> <?= $leave["leave_type"] ?></td>
                             <td> <?= $leave["from_date"] ?></td>
                             <td> <?= $leave["to_date"] ?></td>
-                            <td> <?= $leave["response"] ?></td>
+                            <td> <?= $leave["status"] ?></td>
                             <td>
                                 <a href="print_leave.php?id=<?=$_SESSION['id']?>"><span class="table_icons icon-print2 text-success"></span></a>
                             </td>
@@ -68,7 +68,7 @@ include_once 'includes/side_navbar.php';
                     </div>
                     <div class="card-body">
                         <?= alerts() ?>
-                        <form action="departments.php" method="post">
+                        <form action="leaves.php" method="post">
                             <div class="form-group">
                                 <label for="leave_type">Leave Type:</label>
                                 <select name="leave_type" id="leave_type" class="custom-select">
