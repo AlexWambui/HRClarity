145049d3dad5198ffc220752d2d920ddb99e5edd
Alex <alexwambuik@gmail.com>
2021-04-18 16:46:31 +0300

    Update side_navbar.php
    made the employees link one button instead of a drop down.

--- app/includes/side_navbar.php
+++ app/includes/side_navbar.php
@@ -2,19 +2,13 @@
     <ul class="nav_links">
         <li class="nav_items"><a href="welcome_page.php"><span class="side_nav_icons icon-dashboard"></span> Dashboard</a></li>
         <?php if($_SESSION['user_level'] == 3 or $_SESSION['user_level'] == 2 ): ?>
-            <li class="nav_items">
-                <button class="dropdown-btn side_nav_dropdown"><span class="side_nav_icons icon-group"></span> Employees <span class="icon-caret-down"></span></button>
-                <div class="dropdown-container side_nav_dropdown_container">
-                    <a href="view_users.php">View employees</a>
-                    <a href="register_user.php">Add employee</a>
-                </div>
-            </li>
-            <li class="nav_items"><a href="recruitments.php"><span class="side_nav_icons icon-folder-open"></span> Recruitments</a></li>
+        <li class="nav_items"><a href="users.php"><span class="side_nav_icons icon-users"></span> Employees</a></li>
+        <li class="nav_items"><a href="recruitments.php"><span class="side_nav_icons icon-folder-open"></span> Recruitments</a></li>
         <?php endif; ?>
         <li class="nav_items"><a href="leaves.php"><span class="side_nav_icons icon-time_to_leave"></span> Leaves</a></li>
         <?php if($_SESSION['user_level'] == 3):?>
-            <li class="nav_items"><a href="departments.php"><span class="side_nav_icons icon-group_work"></span> Departments</a></li>
-            <li class="nav_items"><a href="occupations.php"><span class="side_nav_icons icon-work"></span> Occupations</a></li>
+        <li class="nav_items"><a href="departments.php"><span class="side_nav_icons icon-group_work"></span> Departments</a></li>
+        <li class="nav_items"><a href="occupations.php"><span class="side_nav_icons icon-work"></span> Occupations</a></li>
         <?php endif; ?>
         <li class="nav_items"><a href="payslip.php"><span class="side_nav_icons icon-money"></span> Payslip</a></li>
 <!--        --><?php //if($_SESSION['user_level'] == 3 or $_SESSION['user_level'] == 2 ): ?>
