6d3a454fe46337324902bd042bf365b2f7af81eb
Alex <alexwambuik@gmail.com>
2021-04-19 03:16:11 +0300

    styling navs and login_page
    added better styling to the nav bar and the login page

--- app/includes/top_navbar.php
+++ app/includes/top_navbar.php
@@ -1,4 +1,4 @@
-<header class="top_nav navbar-expand-md bg-dark navbar-dark pl-2 pr-2">
+<header class="top_nav navbar-expand-md pl-2 pr-2">
     <nav class="d-flex justify-content-between align-items-center">
         <div class="logo">
             <h4 class="app_name">HRMS</h4>
@@ -6,13 +6,13 @@
         <ul class="navbar-nav">
             <?php if(isset($_SESSION['id'])): ?>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle <?php if($_SESSION['user_level'] == 3) echo 'text-success' ?>" href="#" id="navbardrop" data-toggle="dropdown">
+                <a class="nav-link dropdown-toggle <?php if($_SESSION['user_level'] == 3) echo 'text-warning' ?>" href="#" id="navbardrop" data-toggle="dropdown">
                     <?= $_SESSION["first_name"] ?>
                 </a>
                 <ul class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Your profile</a>
+                    <a class="dropdown-item text-dark" href="#">Your profile</a>
                     <li><hr class="dropdown-divider"></li>
-                    <a class="dropdown-item" href="includes/logout.php">Sign out</a>
+                    <a class="dropdown-item text-dark" href="includes/logout.php">Sign out</a>
                 </ul>
             </li>
             <?php endif; ?>
