a710f14faf6b9edb3783720b9f7409f8f44bc81b
Alex <alexwambuik@gmail.com>
2021-04-19 12:54:02 +0300

    Update top_navbar.php
    used an image instead of plain text for the top nav bar logo. Added more links to the nav link on the right which could be an avatar instead.

--- app/includes/top_navbar.php
+++ app/includes/top_navbar.php
@@ -1,7 +1,8 @@
 <header class="top_nav navbar-expand-md pl-2 pr-2">
     <nav class="d-flex justify-content-between align-items-center">
-        <div class="logo">
-            <h4 class="app_name">HRMS</h4>
+        <div class="logo d-flex flex-row align-items-center">
+            <a href="welcome_page.php" class="m-1"><img src="../assets/images/logo.png" alt="logo"></a>
+<!--            <h4 class="app_name">MS</h4>-->
         </div>
         <ul class="navbar-nav">
             <?php if(isset($_SESSION['id'])): ?>
@@ -10,7 +11,9 @@
                     <?= $_SESSION["first_name"] ?>
                 </a>
                 <ul class="dropdown-menu">
-                    <a class="dropdown-item text-dark" href="#">Your profile</a>
+                    <a class="dropdown-item text-dark" href="leaves.php">Leaves</a>
+                    <a class="dropdown-item text-dark" href="announcements.php">Announcements</a>
+                    <a class="dropdown-item text-dark" href="payslip.php">Payslips</a>
                     <li><hr class="dropdown-divider"></li>
                     <a class="dropdown-item text-dark" href="includes/logout.php">Sign out</a>
                 </ul>
