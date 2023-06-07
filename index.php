29a8ac7f59a69dced5f2f4eecfcf361928c9cf7b
alexwambui <alexwambuik@gmail.com>
2023-06-07 23:49:24 +0300

    added the features and about sections to the home page

--- index.php
+++ index.php
@@ -1,17 +1,97 @@
 <!doctype html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="assets/css/icomoon.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
     <title>HRClarity</title>
 </head>
+
 <body>
+    <nav>
+        <div class="brand">
+            <a href="./index.php">Home</a>
+        </div>
+        <div class="nav_links">
+            <ul>
+                <li><a href="#Features">Features</a></li>
+                <li><a href="#About">About</a></li>
+            </ul>
+        </div>
+    </nav>
+
     <section class="hero_section d-flex flex-row justify-content-center align-items-center">
-        <section class="hero_text justify-content-center">
+        <div class="hero_text justify-content-center">
             <h1 class="text-center">HRClarity</h1>
             <h2 class="text-center mb-5">Human Resource Management made easy.</h2>
             <a href="app/views/login.php" class="btn">Get Started</a>
-        </section>
+        </div>
     </section>
+
+    <section class="Features" id="Features">
+        <div class="features_wrapper">
+            <div class="feature">
+                <div class="details">
+                    <h1>Leaves</h1>
+                    <p>Making leave requests has never been easier. With just a few details to fill in, your request is sent and ready to be reviewed by the HR.</p>
+                </div>
+                <div class="image">
+                    <img src="assets/images/leaves.png" alt="Leaves">
+                </div>
+            </div>
+
+            <div class="feature">
+                <div class="details">
+                    <h1>Announcements</h1>
+                    <p>HR managers can easily make announcements viewable by all employees in the organization.</p>
+                </div>
+                <div class="image">
+                    <img src="assets/images/announcements.png" alt="Announcements">
+                </div>
+            </div>
+
+            <div class="feature">
+                <div class="details">
+                    <h1>Employees</h1>
+                    <p>Verifying, viewing and updating employees data has never been easier. Experience the power of easily searching through all the employees in the organization.</p>
+                </div>
+                <div class="image">
+                    <img src="assets/images/employees.png" alt="Employees">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="About" id="About">
+        <div class="inspiration">
+            <p>The project was inspired by the struggle observed when working with a small organization that needed a HR software but the only available solutions were premium versions of very expensive HR software.</p>
+            <p>Despite the high pricing, the software didn't really offer an efficient way for employees to get registered into the system. Thus the creation of HRClarity to ease the process of employees being registered into the system and also making it easy to perform HR functions such as approving or rejecting leave requests.</p>
+        </div>
+
+        <div class="socials">
+            <div class="team">
+                <p>Team's social profile links:</p>
+                <ul>
+                    <li><a href="https://twitter.com/aaquil_alex" target="_blank"><span class="icon icon-twitter"></span></a></li>
+                    <li><a href="https://github.com/AlexWambui" target="_blank"><span class="icon icon-github"></span></a></li>
+                    <li><a href="https://www.linkedin.com/in/alexwambui/" target="_blank"><span class="icon icon-linkedin"></span></a></li>
+                </ul>
+            </div>
+
+            <div class="project">
+                <p>View the project on Github:</p>
+                <ul>
+                    <li><a href="https://github.com/AlexWambui/HRClarity" target="_blank"><span class="icon icon-github"></span></a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="copyright">
+            <p>&copy; 2023 | All rights reserved | Made by AlexAaqil</p>
+        </div>
+    </footer>
 </body>
 </html>
